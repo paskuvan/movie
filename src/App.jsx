f0e8f80476c23ebaa835b606ea655dc66710778f
paskuvan <majopaskuvan@gmail.com>
2025-03-01 00:07:30 -0300

    tmdb api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import React, { useEffect, useState } from "react"
 import Search from "./components/Search"
-import React, { useState } from "react"
 
+//API 
+
+const API_BASE_URL = "https://api.themoviedb.org/3/discover/movie?api_key=46bd574be3b22dd5299c4e9bbdfa033a";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async() => {
+    try {
+      const response = await fetch(API_BASE_URL, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Failed to fetch movies');
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -17,6 +49,7 @@ const App = () => {
 
        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
        <h1 className="text-white">{searchTerm}</h1>
+       {errorMessage && <p className="error-message">{errorMessage}</p>}
         </div>
     </main>  
   )
